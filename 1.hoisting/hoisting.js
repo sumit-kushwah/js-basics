9019d7c0dd4a09b9b70d6e076917db2d26f41269
UmamaheshMaxwell <maheshmeka16@gmail.com>
2022-02-03 14:36:17 +0530

    added basic javascript

---
+++ 1.hoisting/hoisting.js
@@ -0,0 +1,25 @@
+console.log(a)
+console.log(b)
+var a=10
+var b=20;
+console.log(a)
+console.log(b)
+c()
+function c(name, email){
+    console.log(d)
+    var d=10;
+    console.log(d)
+    
+    console.log(name)
+    console.log(email)
+    console.log('c is called')
+}
+
+
+
+// // inside the execution context (memory)
+// a= undefined
+// b= undefined
+// function c(){
+
+// }
---
+++ app.js
@@ -0,0 +1,5 @@
+var a =10; // Global
+
+function sample(){
+    var a=20; // Local
+}
